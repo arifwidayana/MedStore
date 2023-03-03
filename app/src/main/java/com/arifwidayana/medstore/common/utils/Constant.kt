5d2d8a9c306800ec368579197194c18590851729
Arif Widayana <arifwidayana@gmail.com>
2023-03-03 15:40:24 +0700

    add constant object data store

--- app/src/main/java/com/arifwidayana/medstore/common/utils/Constant.kt
+++ app/src/main/java/com/arifwidayana/medstore/common/utils/Constant.kt
@@ -1,7 +1,11 @@
 package com.arifwidayana.medstore.common.utils
 
 object Constant {
-    //Index
+    // Data Store
+    const val DATASTORE_PREF = "DATASTORE_PREF"
+    const val TOKEN_PREF = "TOKEN"
+
+    // Index
     const val DEFAULT_INDEX_PAGE = 1
     const val NETWORK_PAGE_SIZE = 10
 }
