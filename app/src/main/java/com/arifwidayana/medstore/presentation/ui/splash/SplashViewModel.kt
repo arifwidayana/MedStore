61dc2ff740f4740be22ec3e89cb42ba7c48557a0
Arif Widayana <arifwidayana@gmail.com>
2023-03-04 01:22:38 +0700

    fix bug when error retrieve data

--- app/src/main/java/com/arifwidayana/medstore/presentation/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/arifwidayana/medstore/presentation/ui/splash/SplashViewModel.kt
@@ -4,23 +4,37 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.arifwidayana.medstore.common.wrapper.Resource
 import com.arifwidayana.medstore.data.local.repository.UserPreferenceRepository
+import com.arifwidayana.medstore.data.network.repository.ProductRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val userPreferenceRepository: UserPreferenceRepository
+    private val userPreferenceRepository: UserPreferenceRepository,
+    private val productRepository: ProductRepository
 ): SplashContract, ViewModel() {
     private val _getTokenResult = MutableStateFlow<Resource<Boolean>>(Resource.Empty())
     override val getTokenResult: StateFlow<Resource<Boolean>> = _getTokenResult
 
     override fun getToken() {
         viewModelScope.launch {
-            userPreferenceRepository.getToken().collect {
-                _getTokenResult.value = Resource.Success(it.data?.isNotEmpty())
+            when(productRepository.getDetailProduct(0).first()) {
+                is Resource.Success -> {
+                    userPreferenceRepository.getToken().collect { token ->
+                        _getTokenResult.value = Resource.Success(token.data?.isNotEmpty())
+                    }
+                }
+                is Resource.Error -> {
+                    userPreferenceRepository.logoutUser().first()
+                    userPreferenceRepository.getToken().collect { token ->
+                        _getTokenResult.value = Resource.Success(token.data?.isNotEmpty())
+                    }
+                }
+                else -> {}
             }
         }
     }
