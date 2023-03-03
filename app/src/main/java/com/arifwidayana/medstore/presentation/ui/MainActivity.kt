1ca24f2765ab3f7c71e9efc5a2e674907c42d775
Arif Widayana <arifwidayana@gmail.com>
2023-03-03 21:05:54 +0700

    add logic to change menu fragment

--- app/src/main/java/com/arifwidayana/medstore/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/arifwidayana/medstore/presentation/ui/MainActivity.kt
@@ -2,13 +2,39 @@ package com.arifwidayana.medstore.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.arifwidayana.medstore.R
+import com.arifwidayana.medstore.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.apply {
+            val navController = findNavController(R.id.fragmentContainerView)
+            bottomNavigationView.setupWithNavController(navController)
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.productFragment,
+                    R.id.supplierFragment -> {
+                        bottomNavigationView.visibility = View.VISIBLE
+                    }
+                    else -> {
+                        bottomNavigationView.visibility = View.GONE
+                    }
+                }
+            }
+        }
     }
 }
