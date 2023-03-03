c543ac7769e740f847f8a712ddb0411f9d920d21
Arif Widayana <arifwidayana@gmail.com>
2023-03-03 11:25:31 +0700

    added common base

---
+++ app/src/main/java/com/arifwidayana/medstore/common/base/BaseRepository.kt
@@ -0,0 +1,13 @@
+package com.arifwidayana.medstore.common.base
+
+import com.arifwidayana.medstore.common.wrapper.Resource
+
+abstract class BaseRepository {
+    suspend fun <T> proceed(coroutine: suspend () -> T): Resource<T> {
+        return try {
+            Resource.Success(coroutine.invoke())
+        } catch (exception: Exception) {
+            Resource.Error(exception)
+        }
+    }
+}
